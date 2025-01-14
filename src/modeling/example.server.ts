704a13cb6431a51bd2a8cf7374b19af088095164
shrjrd <153493355+shrjrd@users.noreply.github.com>
2025-01-13 23:39:10 -0600

    Update example Add JSCAD logo

--- src/modeling/example.server.ts
+++ src/modeling/example.server.ts
@@ -1,7 +1,4 @@
-import { Workspace } from "@rbxts/services";
-
 const BasePart = new Instance("Part");
-BasePart.BrickColor = new BrickColor("Bright green");
 BasePart.CanCollide = false;
 BasePart.Anchored = true;
 
@@ -29,7 +26,7 @@ function drawLine3D(a: Vector3, b: Vector3, parent: Instance) {
 	LinePart.Parent = parent;
 }
 
-function drawTriangle3D(a: Vector3, b: Vector3, c: Vector3, parent: Instance) {
+function drawTriangle3D(a: Vector3, b: Vector3, c: Vector3, color: RGB | RGBA, parent: Instance) {
 	let ab = b.sub(a);
 	let ac = c.sub(a);
 	let bc = c.sub(b);
@@ -51,9 +48,11 @@ function drawTriangle3D(a: Vector3, b: Vector3, c: Vector3, parent: Instance) {
 	const w1 = WedgePart.Clone();
 	w1.Size = new Vector3(0, height, math.abs(ab.Dot(back)));
 	w1.CFrame = CFrame.fromMatrix(a.add(b).div(2), right, up, back);
+	w1.Color = new Color3(color[0], color[1], color[2]);
 	const w2 = WedgePart.Clone();
 	w2.Size = new Vector3(0, height, math.abs(ac.Dot(back)));
 	w2.CFrame = CFrame.fromMatrix(a.add(c).div(2), right.mul(-1), up, back.mul(-1));
+	w2.Color = new Color3(color[0], color[1], color[2]);
 	const TriangleModel = new Instance("Model");
 	TriangleModel.Name = "Triangle";
 	w1.Parent = TriangleModel;
@@ -67,7 +66,7 @@ function drawNormal(a: Vector3, b: Vector3, c: Vector3, parent: Instance) {
 	drawLine3D(pos, pos.add(normal.mul(0.25)), parent);
 }
 
-function drawConvexPolygon3D(polygon: Poly3, parent: Instance) {
+function drawConvexPolygon3D(polygon: Poly3, color: RGB | RGBA, parent: Instance) {
 	const vertices = polygon.vertices;
 	for (let i = 2; i < vertices.size(); i++) {
 		const _a = vertices[0];
@@ -77,16 +76,16 @@ function drawConvexPolygon3D(polygon: Poly3, parent: Instance) {
 		const a = new Vector3(_a[0], _a[2], _a[1]);
 		const b = new Vector3(_b[0], _b[2], _b[1]);
 		const c = new Vector3(_c[0], _c[2], _c[1]);
-		drawTriangle3D(a, b, c, parent);
-		drawNormal(a, b, c, parent);
+		drawTriangle3D(a, b, c, color, parent);
+		//drawNormal(a, b, c, parent);
 	}
 }
 
-function drawConvexPolygons3D(polygons: Poly3[], parent: Instance) {
+function drawConvexPolygons3D(polygons: Poly3[], color: RGB | RGBA, parent: Instance) {
 	for (const polygon of polygons) {
 		const PolygonModel = new Instance("Model");
 		PolygonModel.Name = "Polygon";
-		drawConvexPolygon3D(polygon, PolygonModel);
+		drawConvexPolygon3D(polygon, color, PolygonModel);
 		PolygonModel.Parent = parent;
 	}
 }
@@ -94,19 +93,20 @@ function drawConvexPolygons3D(polygons: Poly3[], parent: Instance) {
 function drawGeometry3D(geometry: Geom3, parent: Instance) {
 	const GeometryModel = new Instance("Model");
 	GeometryModel.Name = "Geometry";
-	drawConvexPolygons3D(geometry.polygons, GeometryModel);
+	drawConvexPolygons3D(geometry.polygons, geometry.color ?? [0, 0, 0, 0], GeometryModel);
 	GeometryModel.Parent = parent;
 }
 
-import { subtract } from "./src/operations/booleans";
-import primitives from "./src/primitives";
-{
-	const geom1 = primitives.cube();
-	const geom2 = primitives.cylinder();
-	const geom3 = subtract(geom1, geom2) as Geom3;
-	drawGeometry3D(geom3, Workspace);
-}
-{
-	const geom1 = primitives.torus();
-	drawGeometry3D(geom1, Workspace);
-}
+import { Workspace } from "@rbxts/services";
+
+import { colorize } from "./src/colors";
+import { intersect, subtract } from "./src/operations/booleans";
+import { cube, sphere } from "./src/primitives";
+
+//JSCAD logo
+let outer = subtract(cube({ size: 10 }), sphere({ radius: 6.8 }));
+let inner = intersect(sphere({ radius: 4 }), cube({ size: 7 }));
+outer = colorize([0.65, 0.25, 0.8], outer) as Geom3;
+inner = colorize([0.7, 0.7, 0.1], inner) as Geom3;
+drawGeometry3D(outer, Workspace);
+drawGeometry3D(inner, Workspace);
